1dbe075a4bfc586d5f42b8d3a07a89fd4a6d9837
Hisoka-Kyo <319495298@qq.com>
2025-07-07 09:07:16 +0800

    Update worker.mjs

--- src/api_proxy/worker.mjs
+++ src/api_proxy/worker.mjs
@@ -68,8 +68,8 @@ const handleOPTIONS = async () => {
   });
 };
 
-//const BASE_URL = "https://generativelanguage.googleapis.com";
-const BASE_URL = "https://www.xianjianapi.cn";
+const BASE_URL = "https://generativelanguage.googleapis.com";
+//const BASE_URL = "https://www.xianjianapi.cn";
 const API_VERSION = "v1beta";
 
 // https://github.com/google-gemini/generative-ai-js/blob/cf223ff4a1ee5a2d944c53cddb8976136382bee6/src/requests/request.ts#L71
